ba34a6687094faa7ec519ea7170d00b2607c504e
Godfrey Majwega <majregor@gmail.com>
2017-04-13 16:23:18 -0400

    Initial Commit

---
+++ src/pages/crime-prevention-page/crime-prevention-page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the CrimePreventionPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-crime-prevention-page',
+  templateUrl: 'crime-prevention-page.html',
+})
+export class CrimePreventionPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CrimePreventionPage');
+  }
+
+}
