d6a2df100835818afab754d61bedd7e88314d0c9
Godfrey Majwega <majregor@gmail.com>
2017-05-11 16:18:06 -0400

    Cleaned up assessment module, Started on Improvements module

--- src/pages/improvement/improvement-page/improvement.ts
+++ src/pages/improvement/improvement-page/improvement.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+//Import Pages
+import { 
+          QuestionPage, QuestionsPage, SubtopicPage, TopicPage,
+          AssessmentPage, ImprovementSubtopicPage, ImprovementTopicPage,
+          AboutPage, CrimePreventionPage, IntroPage, UDPage, ToolsPage } from '../../pages';
+          
+import { RemsatApi, SQLStorage, LocalStorage, Loader } from '../../../shared/shared';
+
 @Component({
   selector: 'page-improvement',
   templateUrl: './improvement.html'
 })
 export class ImprovementPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, 
+              public loadingCtr:Loader, 
+              public storage: SQLStorage, 
+              public localStorage: LocalStorage) {}
 
+  topics:any[];
+
+  topicClicked(topic:any):void{
+    
+    this.navCtrl.push(ImprovementSubtopicPage, topic);
   }
+
+   private isMainTopic(element, index, array) { 
+    return (element.parent==0); 
+  } 
+
+  ionViewDidLoad():void{
+
+    let loader = this.loadingCtr.createLoader('Loading...', 'circles', false);
+
+    loader.present()
+    .then(()=>{
+      
+        this.topics = this.storage.getCategories();
+        loader.dismiss();
+
+    })
+    .catch((err)=>{
+      console.log('Error: '+ err);
+    });    
+}
+
 
 }
--- src/pages/improvement/subtopic-page/improvement-subtopic.ts
+++ src/pages/improvement/subtopic-page/improvement-subtopic.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { SQLStorage, LocalStorage, Loader } from '../../../shared/shared';
+
+//Import Pages
+import {  ImprovementPage, ImprovementTopicPage } from '../../pages';
 
 @Component({
   selector: 'page-improvement-subtopic',
@@ -7,8 +12,96 @@ import { NavController } from 'ionic-angular';
 })
 export class ImprovementSubtopicPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private loadingCtrl:Loader, public navParams: NavParams, public localStorage:LocalStorage, public sqlStorage:SQLStorage) {
 
+  }
+
+  topic:any = {};
+  subTopics:any[] = [];
+
+
+  isChild(element, index, array) { 
+      return (element.parent == this ); 
+  }
+
+  getSubTopicQuestions(value, index, array){
+    let _questions:any = this;
+    value.questions = _questions.filter(function(element, index, array){ return (element.cat_id == this); }, value.id);
+    value.hasQuestions = (value.questions.length>0) ? true : false;
+    value.assessmentComplete = (value.hasQuestions) ? checkCompletion(value.questions) : false;
+
+      function checkCompletion(q:any[]):boolean{
+      let complete:boolean = true;
+      for(let i of q){
+        if(!i.answered){
+          complete = false;
+          break;
+        }
+      }
+      return complete;
+    }
+  }
+
+  ionViewWillEnter(){
+    
+    this.subTopics.map((value, index, array)=>{
+      if(value.hasQuestions){
+        this.sqlStorage.checkCompletion(value.id).then((data)=>{
+          value.assessmentComplete = <boolean>data;
+        }).catch((err)=>{alert(err)});
+
+      }
+    }, this);
+    /*for(let subTopic of this.subTopics){
+      if(subTopic.hasQuestions){
+        let complete:boolean = true;
+        for(let qu of subTopic.questions){
+          if(!qu.answered){
+            complete = false;
+            break;
+          }
+        }
+
+        if(complete){
+            let index = this.subTopics.indexOf(subTopic);
+            this.subTopics[index].assessmentComplete = (index !== -1) ? true : false ;
+        }
+
+      }
+    }*/
+
+    
+  }
+   
+  ionViewDidLoad(){
+    this.topic = this.navParams.data;
+
+    let loader = this.loadingCtrl.createLoader('Loading...','circles',false);
+
+    loader.present().then(()=>{
+
+      /*this.localStorage.getTopics().then( (val) => {
+        this.localStorage.getQuestions().then( (v) => { 
+          this.questions = v; 
+          this.subTopics = val.filter( this.isChild, this.topic.id );
+          this.subTopics.map(this.getSubTopicQuestions, this.questions);
+          loader.dismiss();
+        });
+      });*/
+
+      this.subTopics = this.sqlStorage.getCategories(this.topic.id, true);
+
+      loader.dismiss();
+      
+    });
+  }
+
+  subTopicClicked(subtopic:any):void{
+    
+    if(subtopic.hasQuestions){
+      //this.navCtrl.push(QuestionsPage, subtopic);
+    }
+    
   }
 
 }
